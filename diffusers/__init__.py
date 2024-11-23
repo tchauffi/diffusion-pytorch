a2514f6c79e350ae7d98fcdf7181d89610abefd5
Thibaut CHAUFFIER <tchauffi@gmail.com>
2024-11-23 12:34:21 +0100

    Add main module with package metadata for diffusers

---
+++ diffusers/__init__.py
@@ -0,0 +1,5 @@
+"""This module is the main module of the package diffusers."""
+
+__version__ = "0.1.0"
+__author__ = "Thibaut CHAUFFIER"
+__description__ = "A package to implement diffusers using pytorch."
