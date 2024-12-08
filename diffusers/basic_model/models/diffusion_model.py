FIX: Adjust learning rate and normalization in DiffusionModel; refactor attention mechanism in UNet

diffusers/basic_model/models/diffusion_model.py
@@ -16,13 +16,13 @@ from ..schedulers import (
 
 
 class DiffusionModel(pl.LightningModule):
-    def __init__(self, in_channels, out_channels, num_filters, num_steps, lr=1e-3):
+    def __init__(self, in_channels, out_channels, num_filters, num_steps, lr=5e-3):
         super().__init__()
         self.unet = UNet(in_channels, out_channels, num_filters)
         self.num_steps = num_steps
 
-        steps = torch.arange(num_steps, dtype=torch.float32) / num_steps
-        self.alphas, self.betas = offset_cosine_diffusion_scheduler(steps)
+        steps = torch.arange(num_steps, dtype=torch.float32)
+        self.alphas, self.betas = offset_cosine_diffusion_scheduler(steps / num_steps)
         self.criterion = torch.nn.L1Loss(reduction="mean")
         self.ema = ModelEmaV3(self.unet, decay=0.999)
 
@@ -36,7 +36,7 @@ class DiffusionModel(pl.LightningModule):
 
         alpha = self.alphas[t].reshape(x.shape[0], 1, 1, 1).to(x.device)
         beta = self.betas[t].reshape(x.shape[0], 1, 1, 1).to(x.device)
-        noisy_image = alpha * x + beta * noise
+        noisy_image = beta * x + alpha * noise
         pred_noises = self.unet(noisy_image, t / self.num_steps)
         loss = self.criterion(pred_noises, noise)
         self.log("train_loss", loss, prog_bar=True)
@@ -59,7 +59,7 @@ class DiffusionModel(pl.LightningModule):
                 self.ema.eval()
                 pred_noise = self.ema(current_images, t)
 
-                pred_image = (current_images - pred_noise * beta) / alpha
+                pred_image = (current_images - pred_noise * alpha) / beta
 
                 alpha_minus_one = (
                     alphas[step - 1].repeat(num_images).reshape(num_images, 1, 1, 1)
@@ -69,13 +69,13 @@ class DiffusionModel(pl.LightningModule):
                 )
 
                 current_images = (
-                    alpha_minus_one * pred_image + beta_minus_one * pred_noise
+                    beta_minus_one * pred_image + alpha_minus_one * pred_noise
                 )
 
         return pred_image
 
     def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr, weight_decay=1e-4)
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr, weight_decay=1e-6)
         return optimizer
 
     def optimizer_step(self, *args, **kwargs):

diffusers/basic_model/models/unet.py
@@ -1,16 +1,16 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from einops import rearrange
+import math
 
 
-def timestep_embedding(timesteps, dim, max_period=10000):
+def timestep_embedding(timesteps, dim, max_period=1000.0):
     """
     Generate sinusoidal embeddings for the given timesteps.
     """
     half_dim = dim // 2
     frequencies = torch.exp(
-        -torch.log(torch.tensor(max_period, dtype=torch.float32))
+        torch.log(torch.tensor(max_period, dtype=torch.float32))
         * torch.arange(start=0, end=half_dim, dtype=torch.float32)
         / half_dim
     ).to(timesteps.device)
@@ -23,33 +23,27 @@ def timestep_embedding(timesteps, dim, max_period=10000):
     return embeddings
 
 
-class Attention(nn.Module):
-    """
-    This module performs multi-head scaled dot-product attention.
-    """
-
-    def __init__(self, C: int, num_heads: int, dropout_prob: float):
+class AttentionBlock(nn.Module):
+    def __init__(self, channels, num_heads=1):
         super().__init__()
-        self.proj1 = nn.Linear(C, C * 3)
-        self.proj2 = nn.Linear(C, C)
         self.num_heads = num_heads
-        self.dropout_prob = dropout_prob
+        assert channels % num_heads == 0
+
+        self.norm = nn.BatchNorm2d(channels)
+        self.qkv = nn.Conv2d(channels, channels * 3, kernel_size=1, bias=False)
+        self.proj = nn.Conv2d(channels, channels, kernel_size=1)
 
     def forward(self, x):
-        """
-        Forward pass of the attention module.
-        """
-        h, w = x.shape[2:]
-        x = rearrange(x, "b c h w -> b (h w) c")
-        x = self.proj1(x)
-        x = rearrange(x, "b L (C H K) -> K b H L C", K=3, H=self.num_heads)
-        q, k, v = x[0], x[1], x[2]
-        x = F.scaled_dot_product_attention(
-            q, k, v, is_causal=False, dropout_p=self.dropout_prob
-        )
-        x = rearrange(x, "b H (h w) C -> b h w (C H)", h=h, w=w)
-        x = self.proj2(x)
-        return rearrange(x, "b h w C -> b C h w")
+        B, C, H, W = x.shape
+        qkv = self.qkv(self.norm(x))
+        q, k, v = qkv.reshape(B * self.num_heads, -1, H * W).chunk(3, dim=1)
+        scale = 1.0 / math.sqrt(math.sqrt(C // self.num_heads))
+        attn = torch.einsum("bct,bcs->bts", q * scale, k * scale)
+        attn = attn.softmax(dim=-1)
+        h = torch.einsum("bts,bcs->bct", attn, v)
+        h = h.reshape(B, -1, H, W)
+        h = self.proj(h)
+        return h + x
 
 
 class ResidualConvBlock(nn.Module):
@@ -95,9 +89,9 @@ class ResidualConvBlock(nn.Module):
             )
         self.conv1 = nn.Conv2d(out_channels, out_channels, kernel_size, stride, padding)
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size, stride, padding)
-        self.bn1 = nn.GroupNorm(32, out_channels)
-        self.bn2 = nn.GroupNorm(32, out_channels)
-        self.silu = F.silu
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.silu = nn.SiLU()
         self.dropout = nn.Dropout(0.1)
 
     def forward(self, x, embeddings):
@@ -156,7 +150,7 @@ class DownBlock(nn.Module):
         self.conv2 = ResidualConvBlock(out_channels, out_channels)
         self.downsample = nn.MaxPool2d(2)
         if attention:
-            self.attention = Attention(out_channels, num_heads=8, dropout_prob=0.1)
+            self.attention = AttentionBlock(out_channels, num_heads=4)
 
     def forward(self, x, embeddings):
         x = self.conv1(x, embeddings)
@@ -195,7 +189,7 @@ class UpBlock(nn.Module):
         self.conv2 = ResidualConvBlock(out_channels, out_channels)
         self.out_channels = out_channels
         if attention:
-            self.attention = Attention(out_channels, num_heads=8, dropout_prob=0.1)
+            self.attention = AttentionBlock(out_channels, num_heads=4)
 
     def forward(self, x, skip, embeddings):
         x = self.upsample(x)
@@ -238,15 +232,16 @@ class UNet(nn.Module):
         self.max_filters_nbr = num_filters * 8 + num_filters * 4
         self.entry_conv = nn.Conv2d(in_channels, num_filters, 3, 1, 1)
         self.down1 = DownBlock(num_filters, num_filters)
-        self.down2 = DownBlock(num_filters, num_filters * 2, attention=False)
+        self.down2 = DownBlock(num_filters, num_filters * 2, attention=True)
         self.down3 = DownBlock(num_filters * 2, num_filters * 4)
         self.bottleneck1 = ResidualConvBlock(num_filters * 4, num_filters * 8)
         self.bottleneck2 = ResidualConvBlock(num_filters * 8, num_filters * 8)
         self.up3 = UpBlock(num_filters * 8 + num_filters * 4, num_filters * 4)
         self.up2 = UpBlock(
-            num_filters * 4 + num_filters * 2, num_filters * 2, attention=False
+            num_filters * 4 + num_filters * 2, num_filters * 2, attention=True
         )
         self.up1 = UpBlock(num_filters * 2 + num_filters, num_filters)
+        self.end_norm = nn.GroupNorm(num_filters, num_filters)
         self.out = nn.Conv2d(num_filters, out_channels, 1)
         self.time_embedder = timestep_embedding
 
@@ -266,6 +261,7 @@ class UNet(nn.Module):
         x = self.up3(x, skip3, embeds)
         x = self.up2(x, skip2, embeds)
         x = self.up1(x, skip1, embeds)
+        x = self.end_norm(x)
         x = F.silu(x)
         x = self.out(x)
         return x

diffusers/basic_model/train.py
@@ -2,7 +2,6 @@ import torch
 from torch.utils.data import DataLoader
 from torchvision.transforms import v2 as transforms
 import pytorch_lightning as pl
-from pytorch_lightning.callbacks import LearningRateFinder
 from PIL import Image
 from datasets import load_dataset
 from diffusers.basic_model.models.diffusion_model import DiffusionModel
@@ -54,21 +53,23 @@ class CenterCrop(torch.nn.Module):
         return f"{self.__class__.__name__}(size={self.size})"
 
 
-model = DiffusionModel(in_channels=3, out_channels=3, num_filters=64, num_steps=1000)
+model = DiffusionModel(
+    in_channels=3, out_channels=3, num_filters=32, num_steps=1000, lr=1e-3
+)
 model = model.to("mps")
 
 img_callback = DiffusionImageLogger(num_images=4, every_n_epochs=1)
 
-trainer = pl.Trainer(max_epochs=50, callbacks=[img_callback])
+trainer = pl.Trainer(max_epochs=100, callbacks=[img_callback])
 
-dataset = load_dataset("nelorth/oxford-flowers")
+dataset = load_dataset("huggan/flowers-102-categories")
 
 train_dataset = dataset["train"]
 
 transfrom = transforms.Compose(
     [
         CenterCrop(),
-        transforms.Resize((64, 64), Image.BICUBIC),
+        transforms.Resize((64, 64), Image.LANCZOS),
         transforms.RandomHorizontalFlip(),
         transforms.ToImage(),
         transforms.ToDtype(torch.float32, scale=True),